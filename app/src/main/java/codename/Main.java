f21f836e0f17f641b435b2bc92ae17ac7a446abd
lina lekbouri <lina.lekbouri@telecomnancy.eu>
2025-01-07 11:32:59 +0100

    Rajout du menu dans l'application + update de la grille

--- app/src/main/java/codename/Main.java
+++ app/src/main/java/codename/Main.java
@@ -3,28 +3,27 @@ package codename;
 import codename.controleur.AccueilControleur;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+
+import java.util.Objects;
+import java.util.Optional;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Charger le fichier FXML de la vue d'accueil (si vous avez un fichier FXML)
-        // Si vous n'avez pas de FXML, vous pouvez créer l'interface directement en Java comme montré précédemment
-
-        // Par exemple, si vous avez un FXML
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/codename/vue/Accueil.fxml"));
-        Scene scene = new Scene(loader.load());
-
-        // Récupérer le contrôleur depuis l'FXMLLoader
-        AccueilControleur accueilControleur = loader.getController();
-
-        // Passer le primaryStage au contrôleur
-        accueilControleur.setPrimaryStage(primaryStage);
-
-        primaryStage.setTitle("Accueil");
+        //Charger le fichier fxml global
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/codename/vue/Global.fxml")));
+        //Créer la scène
+        Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
+        //Nommer la fenêtre
+        primaryStage.setTitle("Code Name Nitro");
+        //Afficher la fenêtre
         primaryStage.show();
     }
 
--- app/src/main/java/codename/controleur/GlobalControleur.java
+++ app/src/main/java/codename/controleur/GlobalControleur.java
@@ -1,5 +1,19 @@
 package codename.controleur;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * Contrôleur pour la vue principale de l'application : permet de switcher entre les différentes vues.
+ */
 public class GlobalControleur {
 
+    @FXML
+    private BorderPane root;
+
+    // Initialisation du contrôleur après le chargement du fichier FXML
+    @FXML
+    public void initialize() {
+        System.out.println("GlobalControleur initialisé.");
+    }
 }

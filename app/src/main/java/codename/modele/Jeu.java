54b0db7af35457de382b9aa90ca8b608cbc3fbdf
julien <julien.marland@telecomnancy.eu>
2025-01-08 19:55:57 +0100

    corrections du merge

--- app/src/main/java/codename/modele/Jeu.java
+++ app/src/main/java/codename/modele/Jeu.java
@@ -18,7 +18,7 @@ public class Jeu {
     private Equipe equipeBleue;
 
     public Jeu(int taille, int type) {
-        this.grille = new Grille(taille,type);
+        this.grille = new Grille(taille, type);
         this.nbMotsBleu = grille.getNbBleue();
         this.nbMotsRouge = grille.getNbRouge();
         this.tour = 0;
@@ -28,7 +28,7 @@ public class Jeu {
     }
 
     public Jeu() {
-        this.grille = new Grille(5,0);
+        this.grille = new Grille(5, 0);
         this.nbMotsBleu = grille.getNbBleue();
         this.nbMotsRouge = grille.getNbRouge();
         this.tour = 0;
@@ -41,82 +41,107 @@ public class Jeu {
         tour = (tour == 0) ? 1 : 0;
     }
 
-/*
-checkWinner check si l'une des deux équipes a retournée toutes ses cartes.
- */
+    /*
+    checkWinner check si l'une des deux équipes a retournée toutes ses cartes.
+     */
     private void updateRegularWinner() {
         if (nbMotsBleu == 0) {
-            statusPartie=1;
-        }
-        else if (nbMotsRouge == 0) {
-            statusPartie=2;
+            statusPartie = 1;
+        } else if (nbMotsRouge == 0) {
+            statusPartie = 2;
         }
     }
 
-    public boolean isThereWinner(){
+    public boolean isThereWinner() {
         if (statusPartie == 1 || statusPartie == 2) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-/*
-retournerTuile permet de mettre à jour la grille en retournant la carte en position [i][j] si elle ne l'est pas déja.
-elle met ensuite à jour le tour si la carte retournée est celle de l'autre équipe.
-elle met aussi à jour statusPartie en cas de victoire par l'une des 2 équipes, si une a retournée la carte noire ou si une a retournée toutes ses cartes.
- */
-    public int retournerTuile(int i, int j){
-        Tuile theTuile = grille.getTuile(i,j);
+    /*
+    retournerTuile permet de mettre à jour la grille en retournant la carte en position [i][j] si elle ne l'est pas déja.
+    elle met ensuite à jour le tour si la carte retournée est celle de l'autre équipe.
+    elle met aussi à jour statusPartie en cas de victoire par l'une des 2 équipes, si une a retournée la carte noire ou si une a retournée toutes ses cartes.
+     */
+    public int retournerTuile(int i, int j) {
+        Tuile theTuile = grille.getTuile(i, j);
         if (!theTuile.isEstRetournee()) {
             theTuile.setEstRetournee();
-            switch (theTuile.getEquipe()){
+            switch (theTuile.getEquipe()) {
                 case -1 -> {
-                    if (tour==0){statusPartie=2;}
-                    else {statusPartie=1;}
+                    if (tour == 0) {
+                        statusPartie = 2;
+                    } else {
+                        statusPartie = 1;
+                    }
                 }
                 case 0 -> changerTour();
                 case 1 -> {
                     nbMotsBleu--;
-                    if (tour == 1){changerTour();}
+                    if (tour == 1) {
+                        changerTour();
+                    }
                 }
                 case 2 -> {
                     nbMotsRouge--;
-                    if (tour == 0){changerTour();}
+                    if (tour == 0) {
+                        changerTour();
+                    }
                 }
             }
             updateRegularWinner();
         }
         return statusPartie;
     }
-    public void setNbCartes(int nbCartes){
+
+    public void setNbCartes(int nbCartes) {
         this.nbCartes = nbCartes;
     }
-    public int getNbCartes(){
+
+    public int getNbCartes() {
         return nbCartes;
     }
+
     public void setIndice(String indice) {
         this.indice = indice;
     }
-    public String getIndice(){
+
+    public String getIndice() {
         return indice;
     }
-    public Tuile getTuile(int i, int j){
-        return grille.getTuile(i,j);
+
+    public Tuile getTuile(int i, int j) {
+        return grille.getTuile(i, j);
     }
-    public void setTuile(int i, int j, Tuile theTuile){
-        grille.setTuile(i,j,theTuile);
+
+    public void setTuile(int i, int j, Tuile theTuile) {
+        grille.setTuile(i, j, theTuile);
     }
-    public int getTaille(){
+
+    public int getTaille() {
         return grille.getTaille();
     }
-    public void parle(String endroit){
+
+    public void parle(String endroit) {
     }
-    public int getStatusPartie(){return statusPartie;}
-    public int getTour(){return tour;}
-    public Equipe getEquipeBleue() {return equipeBleue;}
-    public Equipe getEquipeRouge() {return equipeRouge;}
+
+    public int getStatusPartie() {
+        return statusPartie;
+    }
+
+    public int getTour() {
+        return tour;
+    }
+
+    public Equipe getEquipeBleue() {
+        return equipeBleue;
+    }
+
+    public Equipe getEquipeRouge() {
+        return equipeRouge;
+    }
 
     public static void sauvegarder(Jeu jeu, String cheminFichier) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
@@ -132,17 +157,8 @@ elle met aussi à jour statusPartie en cas de victoire par l'une des 2 équipes,
         this.taille = taille;
     }
 
-    public Equipe getEquipeRouge() {
-        return equipeRouge;
-    }
+
     public void setEquipeRouge(Equipe equipeRouge) {
         this.equipeRouge = equipeRouge;
-    }
-
-    public Equipe getEquipeBleue() {
-        return equipeBleue;
-    }
-    public void setEquipeBleue(Equipe equipeBleue) {
-        this.equipeBleue = equipeBleue;
     }
 }

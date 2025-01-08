144aad8b07c2c9148269aa9c3850ed1a9da33b2b
Hugo Werck <hugo.werck@telecomnancy.eu>
2025-01-08 20:57:13 +0100

    Petit fix pour que les sauvegardes avec équipes fonctionnent

--- app/src/main/java/codename/modele/Equipe.java
+++ app/src/main/java/codename/modele/Equipe.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 
 public class Equipe {
     private ArrayList<Joueur> joueurs;
+    private int nbJoueurs;
 
     public Equipe() {
         this.joueurs = new ArrayList<>();
     }
-
-
 
     public ArrayList<Joueur> getJoueurs() { return joueurs; }
     public void addJoueur(Joueur joueur) { joueurs.add(joueur); }
@@ -17,4 +16,8 @@ public class Equipe {
 
     public boolean contains(Joueur joueur) { return joueurs.contains(joueur); }
     public int getNbJoueurs() { return joueurs.size(); }
+
+    public void setNbJoueurs(int nbJoueurs) {
+        this.nbJoueurs = nbJoueurs;
+    }
 }
